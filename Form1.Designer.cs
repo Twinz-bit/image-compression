99a6d47e2e5fd6917c277168ee098f6d475f0227
Antonov Eduard <edikantonov2@gmail.com>
2025-06-02 18:26:15 +0300

    first

---
+++ Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace Image_Compression
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(702, 634);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Сжать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(425, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(573, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 20);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Выбрать в ручную";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "10",
+            "20",
+            "30",
+            "40",
+            "50",
+            "60",
+            "70",
+            "80",
+            "90",
+            "100"});
+            this.comboBox1.Location = new System.Drawing.Point(702, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(106, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.Text = "Степень сжатия";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(444, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(123, 20);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Загрузить изображение";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 39);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(683, 618);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(825, 672);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Image Compression";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Image_Compression
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Обработка событий кнопки "Сжать"
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                CompressImageJpeg(textBox1.Text, Convert.ToInt32(comboBox1.Text)); //Сжатие изображения
+                string NewAddressImages = textBox1.Text.Substring(0, textBox1.Text.Length - 4) + $"_сжато_на_{comboBox1.Text}.jpg"; //Путь до сжатого изображения
+                pictureBox1.Image = Image.FromFile(NewAddressImages); //Вывод сжатого изображения на форму
+                MessageBox.Show("Изображение успешно сжато и сохраненно по пути " + NewAddressImages);
+            }
+            catch
+            {
+                MessageBox.Show("Степень сжатия не выбрана или выбранна не коректно");
+            }
+        }
+
+        //Выббор файла в ручную
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var filePath = string.Empty;
+
+            //Открытие проводника
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "C:\\Users\\User\\Pictures"; //Задаёт изначальную папку в проводнике
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Получение пути к указанному файлу
+                    filePath = openFileDialog.FileName;
+                }
+            }
+            textBox1.Text = filePath; //Выводит путь в textBox1 для последующей работы с ним
+        }
+
+        //Загрузка изображения
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                pictureBox1.Image = Image.FromFile(textBox1.Text); //Вывод изображения на форму
+            }
+            catch
+            {
+                MessageBox.Show("Изображение не выбрано или выбрано не коректно");
+            }
+        }
+
+        /// <summary>
+        /// Метод по сжатию изображения Jpeg
+        /// </summary>
+        /// <param name="AddressImages">Путь до изображения</param>
+        /// <param name="compressionRatio">Степень сжатия изображения</param>
+        static void CompressImageJpeg(string AddressImages, long compressionRatio)
+        {
+            using (Bitmap bmp = new Bitmap(AddressImages)) //Объект используемый для работы с изображениями
+                bmp.Save( //Сохранение изображения
+                    Path.ChangeExtension(AddressImages, "").Trim('.') + $"_сжато_на_{compressionRatio}.jpg",
+                    ImageCodecInfo.GetImageEncoders()[1],
+                    new EncoderParameters() //Само сжатие изображения
+                    {
+                        Param = new EncoderParameter[]
+                        {
+                            new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, 100L - compressionRatio)
+                        }
+                    });
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
